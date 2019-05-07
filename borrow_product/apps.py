2ca292b7195c80b8d9cdfa30ea38e0a4b43e8db5
Dominika Karlowicz <dkarlowicz@hotmail.com>
2019-05-07 20:26:12 +0200

    Initial commit

---
+++ borrow_product/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BorrowProductConfig(AppConfig):
+    name = 'borrow_product'
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/coderslab/Pulpit/take_the_game/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
