d39e16c0e21c0a50aeef1876ce75986cbf39391c
Dominika Karlowicz <dkarlowicz@hotmail.com>
2019-05-12 11:16:34 +0200

    create base login view and Profile model

--- sharing_app/models.py
+++ sharing_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+from django.utils.encoding import smart_text
+
+
+class Profile(models.Model):
+	user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+	city = models.CharField(max_length=120)
+	email = models.EmailField
+
+	def __str__(self):
+		return smart_text(self.user.name)
